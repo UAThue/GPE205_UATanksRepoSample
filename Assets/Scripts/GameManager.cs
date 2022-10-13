24058ee5bf92892408e8b88878024b328cb5382e
mhenry@uat.edu <mhenry@uat.edu>
2022-10-13 11:17:08 -0700

    Added Instance Variable to GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    public static GameManager instance; 
+
     // Start is called before the first frame update
     void Start()
     {
